571f02445feb7fb28d1d8ab0fc744e70bd4896a4
Nebi Sarikaya <nebsar@gmail.com>
2018-12-21 00:29:29 -0500

    xml read

---
+++ xmlRead/readXML.cpp
@@ -0,0 +1,23 @@
+#include "readXML.h"
+
+
+readXML::readXML()
+{
+}
+
+
+readXML::~readXML()
+{
+}
+
+void readXML::setFile(std::string fileName) {
+	this->fileName = fileName;
+}
+
+void readXML::readFile() {
+
+}
+
+void readXML::writeStringtoFile() {
+
+}
